fa0fe0215bf430ea1b9873847f1176c7711208c4
IllustriousLoop <70655025+IllustriousLoop@users.noreply.github.com>
2023-01-31 22:49:19 -0500

    add types

--- scripts/tasks.ts
+++ scripts/tasks.ts
@@ -1,85 +1,82 @@
-import path from "path";
-import { promises as fs } from "fs";
-import camelcase from "camelcase";
-import fileFormat from "./templates";
-import { getIconFiles } from "./logics";
-import svgo from "./svgo";
-import { IconDefinition } from "./_types";
-import { icons } from "../src/icons";
+import path from 'path';
+import { promises as fs } from 'fs';
+import camelcase from 'camelcase';
+import fileFormat from './templates';
+import { getIconFiles } from './logics';
+import svgo from './svgo';
+import { IconDefinition } from './_types';
+import { icons } from '../src/icons';
 
 export async function dirInit(home: string) {
-  const ignore = (err) => {
-    if (err.code === "EEXIST") return;
-    throw err;
-  };
-  await fs.mkdir(home, { recursive: true }).catch(ignore);
+	const ignore = (err) => {
+		if (err.code === 'EEXIST') return;
+		throw err;
+	};
+	await fs.mkdir(home, { recursive: true }).catch(ignore);
 
-  const write = (filePath, str) =>
-    fs.writeFile(path.resolve(home, ...filePath), str, "utf8").catch(ignore);
+	const write = (filePath, str) =>
+		fs.writeFile(path.resolve(home, ...filePath), str, 'utf8').catch(ignore);
 
-  const initFiles = ["index.js"];
+	const initFiles = ['index.js', 'index.d.ts'];
 
-  for (const icon of icons) {
-    await fs.mkdir(path.resolve(home, icon.id)).catch(ignore);
+	for (const icon of icons) {
+		await fs.mkdir(path.resolve(home, icon.id)).catch(ignore);
 
-    await write([icon.id, "index.js"], "// THIS FILE IS AUTO GENERATED\n");
-  }
+		await write([icon.id, 'index.js'], '// THIS FILE IS AUTO GENERATED\n');
+	}
 
-  for (const file of initFiles) {
-    await write([file], "// THIS FILE IS AUTO GENERATED\n");
-  }
+	for (const file of initFiles) {
+		if (file === 'index.d.ts') {
+			await write([file], '// THIS FILE IS AUTO GENERATED One\n');
+			continue;
+		}
+		await write([file], '// THIS FILE IS AUTO GENERATED\n');
+	}
 
-  await fs.copyFile(
-    path.resolve(home, "../README.md"),
-    path.resolve(home, "README.md")
-  );
+	await fs.copyFile(path.resolve(home, '../README.md'), path.resolve(home, 'README.md'));
 
-  await fs.copyFile(
-    path.resolve(home, "../LICENSE"),
-    path.resolve(home, "LICENSE")
-  );
+	await fs.copyFile(path.resolve(home, '../LICENSE'), path.resolve(home, 'LICENSE'));
 }
 
 export async function writeIconModuleFiles(icon: IconDefinition, home: string) {
-  const exists = new Set(); // for remove duplicate
+	const exists = new Set(); // for remove duplicate
 
-  for (const content of icon.contents) {
-    const files = await getIconFiles(content);
+	await fs.appendFile(
+		path.resolve(home, icon.id, `index.d.ts`),
+		"// THIS FILE IS AUTO GENERATED\nimport { SvelteComponentTyped } from 'svelte/internal';\n",
+		'utf8'
+	);
+	for (const content of icon.contents) {
+		const files = await getIconFiles(content);
 
-    for (const file of files) {
-      const svgStrRaw = await fs.readFile(file, "utf8");
+		for (const file of files) {
+			const svgStrRaw = await fs.readFile(file, 'utf8');
 
-      const svgClean = svgo(svgStrRaw);
+			const svgClean = svgo(svgStrRaw);
 
-      const rawName = path.basename(file, path.extname(file));
-      const pascalName = camelcase(rawName, { pascalCase: true });
-      const name =
-        (content.formatter && content.formatter(pascalName, file)) ||
-        pascalName;
-      if (exists.has(name)) continue;
-      exists.add(name);
+			const rawName = path.basename(file, path.extname(file));
+			const pascalName = camelcase(rawName, { pascalCase: true });
+			const name = (content.formatter && content.formatter(pascalName, file)) || pascalName;
 
-      const comRes = fileFormat(name, svgClean, "svelte");
-      await fs.writeFile(
-        path.resolve(home, icon.id, `${name}.svelte`),
-        comRes,
-        "utf8"
-      );
+			if (exists.has(name)) continue;
+			exists.add(name);
 
-      const dtsRes = fileFormat(name, svgClean, "fileExport");
-      await fs.appendFile(
-        path.resolve(home, icon.id, `index.js`),
-        dtsRes,
-        "utf8"
-      );
+			const fileSvelte = fileFormat(name, svgClean, 'svelte');
+			await fs.writeFile(path.resolve(home, icon.id, `${name}.svelte`), fileSvelte, 'utf8');
 
-      exists.add(file);
-    }
-    const endRes = fileFormat("", "", "end", icon.id);
-    await fs.appendFile(
-      path.resolve(home, icon.id, `../index.js`),
-      endRes,
-      "utf8"
-    );
-  }
+			const fileExp = fileFormat(name, svgClean, 'fileExport');
+			await fs.appendFile(path.resolve(home, icon.id, `index.js`), fileExp, 'utf8');
+
+			const dts = fileFormat(name, svgClean, 'dts');
+			await fs.writeFile(path.resolve(home, icon.id, `${name}.svelte.d.ts`), dts, 'utf8');
+
+			const dtsIndx = fileFormat(name, svgClean, 'fileExportTS');
+			await fs.appendFile(path.resolve(home, icon.id, `index.d.ts`), dtsIndx, 'utf8');
+
+			exists.add(file);
+		}
+	}
+	const endRes = fileFormat('', '', 'end', icon.id);
+	await fs.appendFile(path.resolve(home, icon.id, `../index.js`), endRes, 'utf8');
+	await fs.appendFile(path.resolve(home, icon.id, `../index.d.ts`), endRes, 'utf8');
 }
--- scripts/templates.ts
+++ scripts/templates.ts
@@ -1,24 +1,57 @@
 export default function fileFormat(
-  formattedName: string,
-  iconData: string,
-  type: string,
-  id?: string
+	formattedName: string,
+	iconData: string,
+	type: string,
+	id?: string
 ) {
-  switch (type) {
-    case "svelte":
-      return (
-        `<script>// THIS FILE IS AUTO GENERATED\n
+	switch (type) {
+		case 'svelte':
+			return (
+				`<script>// THIS FILE IS AUTO GENERATED\n
           export let size= 24;
+          export let color;
         </script>\n` + `${iconData}\n`
-      );
-    case "fileExport":
-      return (
-        `import ${formattedName} from "./${formattedName}.svelte";\n` +
-        `export {${formattedName}};\n`
-      );
-    case "end":
-      return `export * from './${id}';\n`;
-    default:
-      throw new Error(`Unknown type: ${type}`);
-  }
+			);
+		case 'fileExport':
+			return (
+				`import ${formattedName} from "./${formattedName}.svelte";\n` +
+				`export {${formattedName}};\n`
+			);
+		case 'fileExportTS':
+			return `export declare const ${formattedName}: SvelteComponentTyped;\n`;
+		case 'dts':
+			return `
+/** @typedef {typeof __propDef.props}  ${formattedName}Props */
+/** @typedef {typeof __propDef.events} ${formattedName}Events */
+/** @typedef {typeof __propDef.slots}  ${formattedName}Slots */
+export default class ${formattedName} extends SvelteComponentTyped<{
+    [x: string]: any;
+    color?: string;
+    size?: number;
+}, {
+    [evt: string]: CustomEvent<any>;
+}, {}> {
+}
+export type ${formattedName}Props = typeof __propDef.props;
+export type ${formattedName}Events = typeof __propDef.events;
+export type ${formattedName}Slots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {
+        [x: string]: any;
+        color?: string;
+        size?: number;
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export {};
+`;
+		case 'end':
+			return `export * from './${id}';\n`;
+		default:
+			throw new Error(`Unknown type: ${type}`);
+	}
 }
